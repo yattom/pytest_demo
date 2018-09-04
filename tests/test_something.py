2a971b820e26cd59d50cdf962a4f6d542f3f0c54
yattom <tsutomu.yasui@gmail.com>
2018-09-04 10:19:57 +0900

    mock and fixture

--- tests/test_something.py
+++ tests/test_something.py
@@ -36,9 +36,34 @@ def test_request_ok(request_ok):
     r = requests.get('https://example.com')
     assert r.status_code == 200
 
+@pytest.fixture
+def request_response(mocker, request):
+    if hasattr(request, 'param'):
+        code = request.param['status']
+    else:
+        code = 200
+    class DummyRequest:
+        status_code = code
+    mocker.patch('requests.get', return_value=DummyRequest())
+
+@pytest.mark.parametrize('request_response', [{'status': 404}], indirect=True)
+def test_request_not_found(request_response):
+    r = requests.get('https://example.com')
+    assert r.status_code == 404
 
 
+@pytest.fixture
+def request_mocker(mocker):
+    def set_status_code(status_code):
+        code = status_code
+        class DummyRequest:
+            status_code = code
+        mocker.patch('requests.get', return_value=DummyRequest())
+    return set_status_code
 
-
+def test_request_not_found_2(request_mocker):
+    request_mocker(status_code=404)
+    r = requests.get('https://example.com')
+    assert r.status_code == 404
 
 
